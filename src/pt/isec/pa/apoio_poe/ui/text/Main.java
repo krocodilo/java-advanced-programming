bb7d7c261da01a48710d5a2465dc40695c061f94
krocodilo <dcruz98.exe@gmail.com>
2022-06-18 15:18:35 +0100

    start JavaFX app development

--- src/pt/isec/pa/apoio_poe/ui/text/Main.java
+++ src/pt/isec/pa/apoio_poe/ui/text/Main.java
@@ -1,10 +1,14 @@
 package pt.isec.pa.apoio_poe.ui.text;
 
+import javafx.application.Application;
 import pt.isec.pa.apoio_poe.model.fsm.Context;
+import pt.isec.pa.apoio_poe.ui.gui.JavaFXMain;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Application.launch(JavaFXMain.class, args);
 
         Context fsm = new Context();
         UI ui = new UI( fsm );
